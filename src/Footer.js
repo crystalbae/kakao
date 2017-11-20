9261f6dc708df289b5d2c65f1f7251cf26288019
crystalbae <baecry@gmail.com>
2017-11-20 16:02:59 +0900

    added kakaotalk mobile share

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,7 @@ import './css/Footer.css';
 
 class Footer extends Component {
   componentDidMount() {
+    // Facebook 공유
     window.fbAsyncInit = function() {
       window.FB.init({
         appId      : '1551656804901104',
@@ -12,12 +13,41 @@ class Footer extends Component {
       window.FB.AppEvents.logPageView();
     };
     (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "//connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    // KaKaoTalk 공유
+    const script = `
+      <script type='text/javascript'>
+        Kakao.init('1f8322f27d219bb255bfa879cd0845a5');
+        Kakao.Link.createDefaultButton({
+          container: '#kakao-link-btn',
+          objectType: 'feed',
+          content: {
+            title: '카카오뱅크 비상금대출',
+            description: '60초면 충전 끝. 모바일 비상금',
+            imageUrl: 'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+            link: {
+              mobileWebUrl: 'https://www.kakaobank.com/app/products/emergencyLoan/share',
+              webUrl: 'https://www.kakaobank.com/app/products/emergencyLoan/share'
+            }
+          },
+          buttons: [
+            {
+              title: '자세히 보기',
+              link: {
+                mobileWebUrl: 'https://www.kakaobank.com/app/products/emergencyLoan/share',
+                webUrl: 'https://www.kakaobank.com/app/products/emergencyLoan/share'
+              }
+            }
+          ]
+        });
+      </script>
+    `;
   }
 
   facebookShare() {
@@ -33,6 +63,7 @@ class Footer extends Component {
       if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         // mobile 공유
         console.log("mobile");
+
       } else {
         console.log("pc");
         alert("카카오톡 공유하기는 모바일에서 가능합니다.");
@@ -46,7 +77,7 @@ class Footer extends Component {
         <div className="law-text">준법감시인 심의필 17-47(2017.07.17~2018.07.16)</div>
         <p>친구에게 알려주세요!</p>
         <ul className="share">
-          <li><a onClick={this.platformCheck.bind(this)}><span className="kakao"></span></a></li>
+          <li><a id="kakao-link-btn" href="javascript:;" onClick={this.platformCheck.bind(this)}><span className="kakao"></span></a></li>
           <li><a className="fb-xfbml-parse-ignore" href="javascript:window.open('https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.kakaobank.com%2Fapp%2Fproducts%2FemergencyLoan%2Fshare&amp;src=sdkpreparse', '_blank', 'width=600, height=400');"><span className="facebook"></span></a></li>
 
         </ul>
